77a0593222d73597fe176028b19d155a4f7d32a0
Umesh Mehta <120069900+UmeshMehta1@users.noreply.github.com>
2023-08-19 16:42:09 +0545

    second

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,27 @@ app.get("/", (req,resp)=>{
 app.post("/register",async(req,resp)=>{
   let user = new User(req.body);
   let result = await user.save();
+  result = result.toObject()
+  delete result.password;
   resp.send(result)
+})
+
+app.post('/login', async(req,resp)=>{
+  console.log(req.body);
+  if(req.body.password && req.body.email){
+    let user = await User.findOne(req.body).select("-password");
+
+    if(user){
+      resp.send(user)
+  
+  }
+  else{
+    resp.send({result: "No user Found"})
+  }
+  
+  }else{
+    resp.send({result: "No User Found"})
+  }
 })
 
 
